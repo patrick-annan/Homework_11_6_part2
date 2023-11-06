092dfe5b051aad2f2e169c76207f4f2c40cf1186
Patrick Annan <you@example.compatrickannan@hotmail.com>
2023-11-06 17:46:06 -0500

    first push

---
+++ app.js
@@ -0,0 +1,37 @@
+var number1 = 5;
+var number2 = 15;
+function myFunction(number1, number2){
+  return number1 + number2;
+}
+console.log(myFunction)
+
+function subtractFunction(number1, number2){
+  return number1 - number2;
+}
+console.log(subtractFunction)
+
+function multiplyFunction(number1, number2){
+  return number1 * number2;
+}
+console.log(multiplyFunction)
+
+function divisionFunction(number1, number2){
+  return number1 / number2;
+}
+console.log(divisionFunction)
+
+add =() => {
+  return number1 + number2;
+}
+
+subtract () => {
+  return number1 - number2;
+}
+
+multiply () => {
+  return number1 * number2;
+}
+
+divide () => {
+  return number1 / number2;
+}
